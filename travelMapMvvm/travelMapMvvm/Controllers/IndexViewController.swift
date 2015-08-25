19967ad07713ca0f8580dbae9849e634206ea4e6
changqing <chenchangqing198@126.com>
2015-08-25 17:18:46 +0800

    add indexviewcontroller

---
+++ travelMapMvvm/travelMapMvvm/Controllers/IndexViewController.swift
@@ -0,0 +1,38 @@
+//
+//  IndexViewController.swift
+//  travelMapMvvm
+//
+//  Created by green on 15/8/25.
+//  Copyright (c) 2015年 travelMapMvvm. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * 首页控制器
+ */
+class IndexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ travelMapMvvm/travelMapMvvm/Controllers/LeftViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LeftViewController.swift
+//  travelMapMvvm
+//
+//  Created by green on 15/8/25.
+//  Copyright (c) 2015年 travelMapMvvm. All rights reserved.
+//
+
+import UIKit
+
+/**
+ * 侧边栏控制器
+ */
+class LeftViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
