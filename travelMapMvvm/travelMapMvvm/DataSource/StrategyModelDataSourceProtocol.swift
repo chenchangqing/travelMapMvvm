5c683cc4394adb27ff08d288cb090c0ede273a41
chenchangqing <chenchangqing198@126.com>
2015-08-25 22:20:44 +0800

    add queryModelList comment

--- travelMapMvvm/travelMapMvvm/DataSource/StrategyModelDataSourceProtocol.swift
+++ travelMapMvvm/travelMapMvvm/DataSource/StrategyModelDataSourceProtocol.swift
@@ -15,6 +15,15 @@ protocol StrategyModelDataSourceProtocol {
    
     /**
      * 首页查询攻略
-    */
+     * 
+     * @param 攻略主题数组
+     * @param 旅游月份数组
+     * @param 攻略类型数组
+     * @param 攻略排序
+     * @param 行数
+     * @param 攻略ID(查询这个ID以后的数据)
+     *
+     * @return
+     */
     func queryModelList(params: QueryModelListParams01) -> [StrategyModel]
 }
