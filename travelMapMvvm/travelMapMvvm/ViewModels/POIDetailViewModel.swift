7ed3e672c83457fc1878e78fee18c60fe4446a93
chenchangqing <chenchangqing198@126.com>
2015-09-14 01:25:30 +0800

    POI详情page开发

--- travelMapMvvm/travelMapMvvm/ViewModels/POIDetailViewModel.swift
+++ travelMapMvvm/travelMapMvvm/ViewModels/POIDetailViewModel.swift
@@ -8,6 +8,7 @@
 
 import ReactiveCocoa
 import ReactiveViewModel
+import GONMarkupParser
 
 class POIDetailViewModel: RVMViewModel {
    
@@ -34,5 +35,36 @@ class POIDetailViewModel: RVMViewModel {
             self.poiImageViewModel.downloadImageCommand.execute(nil)
         }
         
+    }
+    
+    // MARK: -
+    
+    /**
+     * 排版攻POI详情数据
+     */
+    func getPOITextViewData(poiModel:POIModel) -> (
+        poiDesc:NSMutableAttributedString,
+        poiAddress:NSMutableAttributedString,
+        openTime:NSMutableAttributedString,
+        poiTiket:NSMutableAttributedString
+    ) {
+        
+        // 简介
+        let poiDesc    = "<font size=\"14\">简介："+(poiModel.desc != nil ? poiModel.desc! : "")+"</>"
+        let poiDescAttributedString = GONMarkupParserManager.sharedParser().attributedStringFromString(poiDesc)
+        
+        // 地址
+        let poiAddress = "<font size=\"14\">地址："+(poiModel.address != nil ? poiModel.address! : "")+"</>"
+        let poiAddressAttributedString = GONMarkupParserManager.sharedParser().attributedStringFromString(poiAddress)
+        
+        // 开放时间
+        let openTime   = "<font size=\"14\">开放时间："+(poiModel.openTime != nil ? "  [\(poiModel.openTime!)人喜欢]" : "")+"</>"
+        let openTimeAttributedString = GONMarkupParserManager.sharedParser().attributedStringFromString(openTime)
+        
+        // 门票
+        let poiTiket   = "<font size=\"14\">门票："+(poiModel.ticketPrice != nil ? poiModel.ticketPrice! : "")+"</>"
+        let poiTiketAttributedString = GONMarkupParserManager.sharedParser().attributedStringFromString(poiTiket)
+        
+        return (poiDescAttributedString,poiAddressAttributedString,openTimeAttributedString,poiTiketAttributedString)
     }
 }
--- travelMapMvvm/travelMapMvvm/Views/Controllers/POIDetailViewController.swift
+++ travelMapMvvm/travelMapMvvm/Views/Controllers/POIDetailViewController.swift
@@ -15,9 +15,17 @@ class POIDetailViewController: UITableViewController {
     @IBOutlet weak var poiPicV  : UIImageView!  // POI 图片
     @IBOutlet weak var poiNameL : UILabel!      // POI 名称
     @IBOutlet weak var levelV   : RatingBar!    // POI 评分
-    @IBOutlet weak var textView : UITextView!   // POI 详细
-    @IBOutlet weak var textViewContainer: UIView!
     
+    @IBOutlet weak var poiDescTextView      : UITextView!   // POI 简介
+    @IBOutlet weak var poiAddressTextView   : UITextView!   // POI 地址
+    @IBOutlet weak var poiOpenTimeTextView  : UITextView!   // POI 开放时间
+    @IBOutlet weak var poiTiketTextView     : UITextView!   // POI 票价
+    @IBOutlet weak var textViewContainer    : UIView!
+    
+    @IBOutlet weak var poiDescTextViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var poiAddressTextViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var poiOpenTimeTextViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var poiTiketTextViewHeightConstraint: NSLayoutConstraint!
     // MARK: - View Model
     
     var poiDetailViewModel:POIDetailViewModel!
@@ -28,9 +36,6 @@ class POIDetailViewController: UITableViewController {
         super.viewDidLoad()
         
         setup()
-        
-        textViewContainer.setHeight(800)
-        self.view.layoutIfNeeded()
         
     }
     
@@ -65,6 +70,30 @@ class POIDetailViewController: UITableViewController {
             }
             return nil
         }.ignore(nil) ~> RAC(levelV,"rating")
+        
+        // POI 详细
+        RACObserve(self, "poiDetailViewModel.poiModel").ignore(nil).subscribeNext { (any:AnyObject!) -> Void in
+            
+            // textView width
+            let textViewWidth = UIScreen.mainScreen().bounds.width - 20
+            
+            let attributeStringTuple = self.poiDetailViewModel.getPOITextViewData(any as! POIModel)
+            self.poiDescTextView.attributedText = attributeStringTuple.poiDesc
+            self.poiAddressTextView.attributedText = attributeStringTuple.poiAddress
+            self.poiOpenTimeTextView.attributedText = attributeStringTuple.openTime
+            self.poiTiketTextView.attributedText = attributeStringTuple.poiTiket
+            
+            self.poiDescTextViewHeightConstraint.constant = self.poiDescTextView.height(textViewWidth)
+            self.poiAddressTextViewHeightConstraint.constant = self.poiAddressTextView.height(textViewWidth - 40 - 6)
+            self.poiOpenTimeTextViewHeightConstraint.constant = self.poiOpenTimeTextView.height(textViewWidth)
+            self.poiTiketTextViewHeightConstraint.constant = self.poiTiketTextView.height(textViewWidth)
+            
+            // textViewContainer Height
+            let textViewContainerHeight = self.poiDescTextViewHeightConstraint.constant + self.poiAddressTextViewHeightConstraint.constant + self.poiOpenTimeTextViewHeightConstraint.constant + self.poiTiketTextViewHeightConstraint.constant + 32
+            
+            self.textViewContainer.setHeight(textViewContainerHeight)
+            self.view.layoutIfNeeded()
+        }
     }
 
 }
