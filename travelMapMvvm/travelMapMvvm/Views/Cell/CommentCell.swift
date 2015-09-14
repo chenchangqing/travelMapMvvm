c3483c6ac466df11888c00d7f5663397d6bcbb85
changqing <chenchangqing198@126.com>
2015-09-14 10:37:42 +0800

    增加评论cell

---
+++ travelMapMvvm/travelMapMvvm/Views/Cell/CommentCell.swift
@@ -0,0 +1,18 @@
+//
+//  CommentCell.swift
+//  travelMap
+//
+//  Created by green on 15/7/9.
+//  Copyright (c) 2015年 com.city8. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var levelV: RatingBar!
+    @IBOutlet weak var contentV: UITextView!
+    @IBOutlet weak var headCircleV: GCircleControl!
+
+}
