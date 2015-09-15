9159bea3533f0713886712cd971df56379845dc7
chenchangqing <chenchangqing198@126.com>
2015-09-16 01:15:01 +0800

    开发POI分段现实页面-视图切换动画

--- travelMapMvvm/travelMapMvvm/Views/Controllers/POIContainerController/POIContainerController.swift
+++ travelMapMvvm/travelMapMvvm/Views/Controllers/POIContainerController/POIContainerController.swift
@@ -7,13 +7,102 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class POIContainerController: UIViewController {
+    
+    // MARK: - 切换地图或列表按钮
+    
+    @IBOutlet weak var rightBtn: UIBarButtonItem!
 
+    // MARK: - Constant
+    
+    private let kMapModeBtnTitle    = "地图"
+    private let kListModeBtnTitle   = "列表"
+    
+    // MARK: - 子视图控制器
+    
+    private var mapViewController   : UIViewController!
+    private var listViewController  : UIViewController!
+    
+    // MARK: - 当前显示的视图控制器
+    
+    private var currentViewController: UIViewController!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUp()
+    }
+    
+    // MARK: - Set Up
+    
+    private func setUp() {
+        
+        setUpTitle()
+        setUpCurrentViewController()
+        setUpChildViewControllers()
+    }
+    
+    // MARK: - Set Up Title
+    
+    private func setUpTitle() {
+     
+        self.title = kListModeBtnTitle
+    }
+    
+    // MARK: - Set Up CurrentViewController
+    
+    private func setUpCurrentViewController() {
+        
+        self.currentViewController = self.childViewControllers[1] as! UIViewController
+    }
+    
+    // MARK: - Set Up ChildViewControllers
+    
+    private func setUpChildViewControllers() {
+        
+        self.mapViewController  = self.childViewControllers[0] as! UIViewController
+        self.listViewController = self.childViewControllers[1] as! UIViewController
+    }
+    
+    // MARK: - Set Up Event
+    
+    @IBAction func rightBtnClicked(sender:UIBarButtonItem) {
+        
+        var willShowViewController: UIViewController!
+        
+        // 切换至列表模式
+        if self.title == self.kListModeBtnTitle {
+            
+            willShowViewController = self.mapViewController
+            
+            self.title = kMapModeBtnTitle
+        }
+        
+        else
+        
+        // 切换至地图模式
+        if self.title == self.kMapModeBtnTitle {
+            
+            willShowViewController = self.listViewController
+            
+            self.title = kListModeBtnTitle
+        }
+        
+        // 动画切换
+        self.transitionFromViewController(self.currentViewController, toViewController: willShowViewController, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, animations: { () -> Void in
+            
+        }, completion: { (finished) -> Void in
+            
+            if finished {
+               
+                willShowViewController.didMoveToParentViewController(self)
+                self.currentViewController = willShowViewController
+            }
+        })
     }
 
 }
