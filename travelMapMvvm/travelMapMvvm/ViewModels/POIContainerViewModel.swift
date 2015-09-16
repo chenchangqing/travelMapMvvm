8f531c59505e282b4ac01258b43b0170c53834bb
chenchangqing <chenchangqing198@126.com>
2015-09-17 02:03:46 +0800

    优化

--- travelMapMvvm/travelMapMvvm/ViewModels/POIContainerViewModel.swift
+++ travelMapMvvm/travelMapMvvm/ViewModels/POIContainerViewModel.swift
@@ -26,6 +26,14 @@ class POIContainerViewModel: RVMViewModel {
     
     private let poiModelDataSourceProtocol = JSONPOIModelDataSource.shareInstance()
     
+    // MARK: - Map POI 控制器字典
+    
+    var poiMapControllerDic = [POITypeEnum:POIMapViewController]()
+    
+    // MARK: - List POI 控制器字典
+    
+    var poiListControllerDic = [POITypeEnum:POIListViewController]()
+    
     // MARK: - Init
     
     init(paramTuple: (queryType:QueryTypeEnum,param:String)) {
@@ -35,6 +43,9 @@ class POIContainerViewModel: RVMViewModel {
         super.init()
         
         setUpCommands()
+        setUpPOIListControllerDic()
+        setUpPOIMapControllerDic()
+        bindDataBetweenMapPOIViewControllerAndListPOIViewController()
     }
     
     // MARK: - Set Up Commands
@@ -131,5 +142,152 @@ class POIContainerViewModel: RVMViewModel {
             break;
         }
         
+    }
+    
+    // MARK: - Set Up Map/List Controllers
+    
+    /**
+     * Map POI 控制器字典
+     */
+    private func setUpPOIMapControllerDic() {
+        
+        for item in enumerate(POITypeEnum.allValues) {
+            
+            let poiMapViewController = UIViewController.getViewController("POIMap", identifier: "POIMapViewController") as! POIMapViewController
+            
+            // Map数据来源是绑定列表数据，这里不用查询
+            let emptyCommand = RACCommand(signalBlock: { (any:AnyObject!) -> RACSignal! in
+                
+                return RACSignal.empty().materialize()
+            })
+            
+            switch(item.element) {
+                
+            case .Scenic:
+                
+                poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListScenicCommand)
+                poiMapViewController.title = POITypeEnum.Scenic.rawValue
+                
+                poiMapControllerDic[.Scenic] = poiMapViewController
+                
+                break
+                
+            case .Food:
+                
+                poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListFoodCommand)
+                poiMapViewController.title = POITypeEnum.Food.rawValue
+                
+                poiMapControllerDic[.Food] = poiMapViewController
+                
+                break
+                
+            case .Shopping:
+                
+                poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListShoppingCommand)
+                poiMapViewController.title = POITypeEnum.Shopping.rawValue
+                
+                poiMapControllerDic[.Shopping] = poiMapViewController
+                
+                break
+                
+            case .Hotel:
+                
+                poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListHotelCommand)
+                poiMapViewController.title = POITypeEnum.Hotel.rawValue
+                
+                poiMapControllerDic[.Hotel] = poiMapViewController
+                
+                break
+                
+            case .Activity:
+                
+                poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListActivityCommand)
+                poiMapViewController.title = POITypeEnum.Activity.rawValue
+                
+                poiMapControllerDic[.Activity] = poiMapViewController
+                
+                break
+            default:
+                
+                break
+            }
+        }
+    }
+    
+    /**
+     * List POI 控制器字典
+     */
+    private func setUpPOIListControllerDic() {
+        
+        for item in enumerate(POITypeEnum.allValues) {
+            
+            let poiListViewController = UIViewController.getViewController("POIList", identifier: "POIListViewController") as! POIListViewController
+            
+            switch(item.element) {
+                
+            case .Scenic:
+                
+                poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListScenicCommand, loadmoreCommand: queryPOIListScenicCommand)
+                poiListViewController.title = POITypeEnum.Scenic.rawValue
+                
+                poiListControllerDic[.Scenic] = poiListViewController
+                
+                break
+                
+            case .Food:
+                
+                poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListFoodCommand, loadmoreCommand: queryPOIListFoodCommand)
+                poiListViewController.title = POITypeEnum.Food.rawValue
+                
+                poiListControllerDic[.Food] = poiListViewController
+                
+                break
+                
+            case .Shopping:
+                
+                poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListShoppingCommand, loadmoreCommand: queryPOIListShoppingCommand)
+                poiListViewController.title = POITypeEnum.Shopping.rawValue
+                
+                poiListControllerDic[.Shopping] = poiListViewController
+                
+                break
+                
+            case .Hotel:
+                
+                poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListHotelCommand, loadmoreCommand: queryPOIListHotelCommand)
+                poiListViewController.title = POITypeEnum.Hotel.rawValue
+                
+                poiListControllerDic[.Hotel] = poiListViewController
+                
+                break
+                
+            case .Activity:
+                
+                poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListActivityCommand, loadmoreCommand: queryPOIListActivityCommand)
+                poiListViewController.title = POITypeEnum.Activity.rawValue
+                
+                poiListControllerDic[.Activity] = poiListViewController
+                
+                break
+                
+            default:
+                
+                break
+            }
+        }
+    }
+    
+    // MARK: - MapPOI控制器数据 与 ListPOI控制器数据 保持一致
+    
+    private func bindDataBetweenMapPOIViewControllerAndListPOIViewController() {
+        
+        for item in enumerate(POITypeEnum.allValues) {
+            
+            let poiMapViewController    = poiMapControllerDic[item.element]!
+            let poiListViewController   = poiListControllerDic[item.element]!
+            
+//            RACObserve(poiListViewController.poiListViewModel, "poiList") ~> RAC(poiMapViewController.poiMapViewModel,"poiList")
+//            RACObserve(poiMapViewController.poiMapViewModel, "poiList") ~> RAC(poiListViewController.poiListViewModel,"poiList")
+        }
     }
 }
--- travelMapMvvm/travelMapMvvm/Views/Controllers/POIContainerController/POITypeViewController.swift
+++ travelMapMvvm/travelMapMvvm/Views/Controllers/POIContainerController/POITypeViewController.swift
@@ -34,128 +34,23 @@ class POITypeViewController: THSegmentedPager {
     
     private func setUpContentContainer() {
         
-        let queryPOIListScenicCommand   = self.poiTypeViewModel.poiContainerViewModel.queryPOIListScenicCommand
-        let queryPOIListFoodCommand     = self.poiTypeViewModel.poiContainerViewModel.queryPOIListFoodCommand
-        let queryPOIListShoppingCommand = self.poiTypeViewModel.poiContainerViewModel.queryPOIListShoppingCommand
-        let queryPOIListHotelCommand    = self.poiTypeViewModel.poiContainerViewModel.queryPOIListHotelCommand
-        let queryPOIListActivityCommand = self.poiTypeViewModel.poiContainerViewModel.queryPOIListActivityCommand
-        
-        var pages = [UIViewController]()
-        
         switch (self.poiTypeViewModel.showPoiMode) {
             
         case .Map:  // 地图模式
             
-            for item in enumerate(self.poiTypeViewModel.segmentedControlItems) {
-                
-                let poiMapViewController = UIViewController.getViewController("POIMap", identifier: "POIMapViewController") as! POIMapViewController
-                pages.append(poiMapViewController)
-                
-                switch(item.element) {
-                    
-                case .Scenic:
-                    
-//                    let tempC = RACCommand(signalBlock: { (any:AnyObject!) -> RACSignal! in
-//                        
-//                        return RACSignal.empty().materialize()
-//                    })
-//                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: tempC)
-                    
-                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListScenicCommand)
-                    poiMapViewController.title = POITypeEnum.Scenic.rawValue
-                    
-                    break
-                    
-                case .Food:
-                    
-                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListFoodCommand)
-                    poiMapViewController.title = POITypeEnum.Food.rawValue
-                    
-                    break
-                    
-                case .Shopping:
-                    
-                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListShoppingCommand)
-                    poiMapViewController.title = POITypeEnum.Shopping.rawValue
-                    
-                    break
-                    
-                case .Hotel:
-                    
-                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListHotelCommand)
-                    poiMapViewController.title = POITypeEnum.Hotel.rawValue
-                    
-                    break
-                    
-                case .Activity:
-                    
-                    poiMapViewController.poiMapViewModel = POIMapViewModel(searchPOIListCommand: queryPOIListActivityCommand)
-                    poiMapViewController.title = POITypeEnum.Activity.rawValue
-                    
-                    break
-                default:
-                    
-                    break
-                }
-            }
+            self.pages = NSMutableArray(array: self.poiTypeViewModel.poiContainerViewModel.poiMapControllerDic.values.array)
             
             break;
         case .List: // 列表模式
             
-            for item in enumerate(self.poiTypeViewModel.segmentedControlItems) {
-                
-                let poiListViewController = UIViewController.getViewController("POIList", identifier: "POIListViewController") as! POIListViewController
-                pages.append(poiListViewController)
-                
-                switch(item.element) {
-                    
-                case .Scenic:
-                    
-                    poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListScenicCommand, loadmoreCommand: queryPOIListScenicCommand)
-                    poiListViewController.title = POITypeEnum.Scenic.rawValue
-                    
-                    break
-                    
-                case .Food:
-                    
-                    poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListFoodCommand, loadmoreCommand: queryPOIListFoodCommand)
-                    poiListViewController.title = POITypeEnum.Food.rawValue
-                    
-                    break
-                    
-                case .Shopping:
-                    
-                    poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListShoppingCommand, loadmoreCommand: queryPOIListShoppingCommand)
-                    poiListViewController.title = POITypeEnum.Shopping.rawValue
-                    
-                    break
-                    
-                case .Hotel:
-                    
-                    poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListHotelCommand, loadmoreCommand: queryPOIListHotelCommand)
-                    poiListViewController.title = POITypeEnum.Hotel.rawValue
-                    
-                    break
-                    
-                case .Activity:
-                    
-                    poiListViewController.poiListViewModel = POIListViewModel(refreshCommand: queryPOIListActivityCommand, loadmoreCommand: queryPOIListActivityCommand)
-                    poiListViewController.title = POITypeEnum.Activity.rawValue
-                    
-                    break
-                    
-                default:
-                    
-                    break
-                }
-            }
+            self.pages = NSMutableArray(array: self.poiTypeViewModel.poiContainerViewModel.poiListControllerDic.values.array)
             
             break;
             
         default:
+            
             break;
         }
-        self.pages = NSMutableArray(array: pages)
     }
 }
 
