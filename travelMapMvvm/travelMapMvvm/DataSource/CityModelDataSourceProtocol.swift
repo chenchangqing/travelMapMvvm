01991178ca9ce4828dd84cbd6fa7b1b6c0546f47
chenchangqing <chenchangqing198@126.com>
2015-09-18 00:33:10 +0800

    temp ci

---
+++ travelMapMvvm/travelMapMvvm/DataSource/CityModelDataSourceProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  CityModelDataSourceProtocol.swift
+//  travelMapMvvm
+//
+//  Created by green on 15/9/18.
+//  Copyright (c) 2015年 travelMapMvvm. All rights reserved.
+//
+
+import Foundation
+
+protocol CityModelDataSourceProtocol {
+    
+    /**
+     * 城市列表
+     * 
+     * @param keyword 关键字
+     */
+}
--- travelMapMvvm/travelMapMvvm/Models/CityModel.swift
+++ travelMapMvvm/travelMapMvvm/Models/CityModel.swift
@@ -6,23 +6,23 @@
 //  Copyright (c) 2015年 com.city8. All rights reserved.
 //
 
-import UIKit
-
 // 城市信息
-//class CityModel: NSObject, Deserializable {
-//   
-//    var cityId: String?      // ID
-//    var cityName: String?    // 城市名称
-//    var cityEnName: String?  // 英文名
-//    var cityPicUrl: String?  // 城市图片地址
-//    var country: String?     // 国家
-//    
-//    init(dictionary: [String: String]) {
-//        
-//        self.cityId = dictionary[kCityId]
-//        self.cityName = dictionary[kCityName]
-//        self.cityEnName = dictionary[kCityEnName]
-//        self.cityPicUrl = dictionary[kCityPicUrl]
-//        self.country = dictionary[kCountry]
-//    }
-//}
+class CityModel: NSObject, Deserializable {
+   
+    var cityId: String?      // ID
+    var cityName: String?    // 城市名称
+    var cityEnName: String?  // 英文名
+    var cityPicUrl: String?  // 城市图片地址
+    var country: String?     // 国家
+    
+    override init() { }
+    
+    required init(data : [String:AnyObject]) {
+        
+        cityId           <-- data["cityId"]
+        cityName         <-- data["cityName"]
+        cityEnName       <-- data["cityEnName"]
+        cityPicUrl       <-- data["cityPicUrl"]
+        country          <-- data["country"]
+    }
+}
